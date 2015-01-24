685e800514f98dbf6d3b9858fc125b9d1afb1278
pallat <yod.pallat@gmail.com>
2015-01-24 16:33:37 +0700

    action new

--- xtend.go
+++ xtend.go
@@ -7,49 +7,52 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/mitchellh/mapstructure"
 	"golang.org/x/net/websocket"
 )
 
 var users []Player
+
+type Event struct {
+	Action string                 `json:"action"`
+	Data   map[string]interface{} `json:"data,omitempty"`
+}
 
 type Player struct {
 	Name string `json:"name"`
 	Conn *websocket.Conn
 }
 
-type Message struct {
-	Sender string `json:"sender"`
-	Text   string `json:"text"`
-}
-
 func Start(ws *websocket.Conn) {
-	var player Player
-	for websocket.JSON.Receive(ws, &player) != io.EOF {
-		player.Conn = ws
-		users = append(users, player)
-		fmt.Println(player.Name, " logged in")
+	var event Event
+	//for websocket.JSON.Receive(ws, &player) != io.EOF {
+	for websocket.JSON.Receive(ws, &event) != io.EOF {
 
-		if (len(users)) >= 2 {
-			fmt.Println("Total users is ", len(users), " game start")
-			if err := websocket.JSON.Send(users[0].Conn, users[1].Name); err != nil {
-				fmt.Println(err.Error())
-			}
-			if err := websocket.JSON.Send(users[1].Conn, users[0].Name); err != nil {
-				fmt.Println(err.Error())
-			}
-		}
+		switch event.Action {
+		case "new":
+			var player Player
+			err := mapstructure.Decode(event.Data, &player)
+			if err != nil {
 
-	}
-}
-
-func Boardcast(ws *websocket.Conn) {
-	var msg Message
-	for websocket.JSON.Receive(ws, &msg) != io.EOF {
-		for i := range users {
-			if users[i].Name != msg.Sender {
-				websocket.JSON.Send(users[i].Conn, msg.Text)
-				fmt.Printf("%s Boardcast %s ======> %s\n", msg.Sender, msg.Text, users[i].Name)
 			}
+			player.Conn = ws
+			users = append(users, player)
+			fmt.Println(player.Name, " logged in")
+			if (len(users)) >= 2 {
+
+				fmt.Println("Total users is ", len(users), " game start")
+
+				if err := websocket.JSON.Send(users[0].Conn, users[1].Name); err != nil {
+					fmt.Println(err.Error())
+				}
+				if err := websocket.JSON.Send(users[1].Conn, users[0].Name); err != nil {
+					fmt.Println(err.Error())
+				}
+			} else {
+				websocket.JSON.Send(player.Conn, Event{Action: "init"})
+			}
+
+		case "":
 		}
 
 	}
