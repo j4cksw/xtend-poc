355e79c4e6df84080928d456833931b30f1f07a5
Prayoch Rujira <jay@odd-e.co.th>
2015-01-24 14:05:07 +0700

    refactored drawing circles as bases

--- web/js/game.js
+++ web/js/game.js
@@ -1,24 +1,22 @@
 var game;
+var gameInfo;
 
-function initGame(){
+function initGame(gameInfo){
+    gameInfo = gameInfo;
     game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
         create: create
     });
 }
 
 function create(){
+    var bases = [];
+    bases.push(drawBase(0xFFFF0B, 200, 300, 100));
+    bases.push(drawBase(0xBC1C22, 600, 300, 100));
+}
 
-    //var base = new Phaser.Circle(400, 300)
-
+function drawBase(color, x, y, size){
     var base = game.add.graphics(0, 0);
-    base.beginFill(0xFFFF0B, 1);
-    base.drawCircle(200, 300, 100);
-
-    var base_2 = game.add.graphics(0, 0);
-    base_2.beginFill(0xBC1C22, 1);
-    base_2.drawCircle(600, 300, 100);
-    console.log(base);
-    console.log(base_2);
-
-    console.log(base === base_2);
+    base.beginFill(color, 1);
+    base.drawCircle(x, y, size);
+    return base;
 }
