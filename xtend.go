3d29e3229a1a5d020fa55a7d4159c3bf5c4fe821
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-01-24 13:49:26 +0700

    move server.go to xtend.go to root folder

---
+++ xtend.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"net/http"
+)
+
+var users []Player
+
+type Player struct {
+	Name string `json:"name"`
+	Conn *websocket.Conn
+}
+
+func Login(ws *websocket.Conn) {
+	var player Player
+	fmt.Println(websocket.JSON.Receive(ws, &player))
+	player.Conn = ws
+	users = append(users, player)
+
+	if (len(users)) == 2 {
+		websocket.JSON.Send(users[0].Conn, users[1].Name)
+		websocket.JSON.Send(users[1].Conn, users[0].Name)
+	}
+	fmt.Println(player)
+}
+
+func main() {
+	http.Handle("/login", websocket.Handler(Login))
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
