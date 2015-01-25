4b67ad2462b6040ba76c94bbcc0b642f486ac699
Prayoch Rujira <jay@odd-e.co.th>
2015-01-25 10:54:51 +0700

    add robot script for help opening web for test game

--- web/js/game.js
+++ web/js/game.js
@@ -3,25 +3,19 @@ var gameInfo;
 var bases;
 
 function initGame(gameInfo){
-    //gameInfo = gameInfo;
     game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
-        create: create
+        create: create,
+        update: update
     });
-    console.log(game.add);
+
     function create(){
-        // var bases = [];
-        // bases.push(drawBase(0xFFFF0B, 100, 300, 100));
-        // bases.push(drawBase(0xBC1C22, 700, 300, 100));
-        //
-        // bases.forEach(function(base){
-        //     base.timer.start();
-        // });
-        // ws.send(JSON.stringify({
-        //     "action":"render_base",
-        //     "data":{
-        //         player:gameInfo.player,
-        //     }
-        // }));
+
     }
 
+    function update(){
+        if (game.input.activePointer.isDown)
+        {
+            console.log(game.input.activePointer);
+        }
+    }
 }
